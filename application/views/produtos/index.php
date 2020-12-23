700d9ea1d79134b904c7466b0b79c60ba02c65f4
luizdogado <luizeduardodogado@gmail.com>
2020-12-22 23:18:55 -0300

    colocando class row para deixar cadastro e login lado a lado

--- application/views/produtos/index.php
+++ application/views/produtos/index.php
@@ -2,68 +2,76 @@
     <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
-    <body>
-        <div class="container">
+        
+        <body>
+            <div class="container">
 
-            <?php if ($this->session->flashdata("success")) :?>       
-            <p class = "alert alert-success"><?= $this->session->flashdata("success") ?></p>
-            <?php endif ?>
-            <?php if ($this->session->flashdata("danger")) :?> 
-            <p class = "alert alert-danger"><?= $this->session->flashdata("danger") ?></p>
-            <?php endif ?>
-            
-            <h1>Produtos</h1>
-            <table class="table">
-            <?php foreach($produtos as $produto) : ?>
-                <tr>
-                    <td><?= $produto["nome"]?></td>
-                    <td><?= numeroEmReais($produto["preco"])?></td>
-                </tr>
-            <?php endforeach ?>
-            </table>  
+                <?php if ($this->session->flashdata("success")) :?>       
+                <p class = "alert alert-success"><?= $this->session->flashdata("success") ?></p>
+                <?php endif ?>
+                <?php if ($this->session->flashdata("danger")) :?> 
+                <p class = "alert alert-danger"><?= $this->session->flashdata("danger") ?></p>
+                <?php endif ?>
+                
+                <h1>Produtos</h1>
+                <table class="table">
+                <?php foreach($produtos as $produto) : ?>
+                    <tr>
+                        <td><?= $produto["nome"]?></td>
+                        <td><?= numeroEmReais($produto["preco"])?></td>
+                    </tr>
+                <?php endforeach ?>
+                </table>  
 
-            <?php if ($this->session->userdata("usuario_logado")) : ?>
-                <a method="post" class="btn btn-primary" href="http://localhost:8080/produtos/formulario">Novo Produto</a>
+                <?php if ($this->session->userdata("usuario_logado")) : ?>
+                    <a method="post" class="btn btn-primary" href="http://localhost:8080/produtos/formulario">Novo Produto</a>
 
 
-                <a method="post" class="btn btn-primary" href="http://localhost:8080/login/logout">Logout</a>
+                    <a method="post" class="btn btn-primary" href="http://localhost:8080/login/logout">Logout</a>
 
-                <?php else : ?>
+                    <?php else : ?>
 
-            <h1> Login </h1>
-            <form method="post" action="http://localhost:8080/login/autenticar" enctype="multipart/form-data" >
-            <div class="form-control">
-                    <h5>Email</h5>
-                    <input name="email" class="form-control" type="text" size="50">
+                <div class="container">
+                <div class="row">
+                <div class="col">
+                <h1>Cadastro</h1>
+                <form method="post" action="http://localhost:8080/usuarios/novo" enctype="multipart/form-data" >
+                    <div class="form-control">
+                        <h5>Nome</h5>
+                        <input name="nome" class="form-control" type="text" size="50">
 
-                    <h5>Senha</h5>
-                    <input name="senha" class="form-control" type="password" size="50">
+                        <h5>Email</h5>
+                        <input name="email" class="form-control" type="text" size="50">
+
+                        <h5>Senha</h5>
+                        <input name="senha" class="form-control" type="password" size="50">
+                    </div>
+
+                    <div class="text-center mt-3">
+                        <button type="submit" class="btn btn-primary" title="Salvar dados" data-tooltip="true">Cadastrar</button>
+                    </div>
+                </form>
+
                 </div>
+                <div class="col">
 
-                <div class="text-center mt-3">
-                    <button type="submit" class="btn btn-primary" title="Salvar dados" data-tooltip="true">Login</button>
-                </div>
-            </form>
-
-            <h1>Cadastro</h1>
-            <form method="post" action="http://localhost:8080/usuarios/novo" enctype="multipart/form-data" >
+                <h1> Login </h1>
+                <form method="post" action="http://localhost:8080/login/autenticar" enctype="multipart/form-data" >
                 <div class="form-control">
-                    <h5>Nome</h5>
-                    <input name="nome" class="form-control" type="text" size="50">
+                        <h5>Email</h5>
+                        <input name="email" class="form-control" type="text" size="50">
 
-                    <h5>Email</h5>
-                    <input name="email" class="form-control" type="text" size="50">
+                        <h5>Senha</h5>
+                        <input name="senha" class="form-control" type="password" size="50">
+                    </div>
 
-                    <h5>Senha</h5>
-                    <input name="senha" class="form-control" type="password" size="50">
+                    <div class="text-center mt-3">
+                        <button type="submit" class="btn btn-primary" title="Salvar dados" data-tooltip="true">Login</button>
+                    </div>
+                </form>
+
+                <?php endif ?>
                 </div>
-
-                <div class="text-center mt-3">
-                    <button type="submit" class="btn btn-primary" title="Salvar dados" data-tooltip="true">Cadastrar</button>
-                </div>
-
-            </form>
-            <?php endif ?>
-        </div>
+            </div>
     </body>
 </html>
